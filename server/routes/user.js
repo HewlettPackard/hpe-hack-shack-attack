62f6816d402d500e3298d7350a02811ff8ef1a61
iankbovard <you@example.com>
2019-04-18 10:26:47 -0500

    added route to send user data to database

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import User from '../models/user.js';
 
 const router = express.Router();
 
-router.get('/user', (req,res) => {
+router.get('/user/leaderboard', (req,res) => {
   res.send({});
 });
 
+router.post('/user/create', (req, res) => {
+  return User.create(req.body)
+    .then(user => res.send(user));
+});
 
 export default router;
