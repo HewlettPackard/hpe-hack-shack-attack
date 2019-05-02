1b2c375b930434e7c162d1d6b49d353982b3233b
iankbovard <you@example.com>
2019-05-02 14:53:10 -0500

    added walls and border to game map and door

--- game-client/src/config/config.js
+++ game-client/src/config/config.js
@@ -7,7 +7,7 @@ export default {
   physics: {
     default: 'arcade',
     arcade: {
-      debug: true,
+      debug: false,
       gravity: { y: 0 }
     }
   }
--- game-client/src/scenes/GameScene.js
+++ game-client/src/scenes/GameScene.js
@@ -22,7 +22,8 @@ export default class GameScene extends Phaser.Scene {
     this.startRound = false;
   }
   create() {
-    this.add.sprite(this.width / 2, this.height / 2, 'map');
+    this.map = this.add.sprite(this.width / 2, this.height / 2, 'map');
+
     this.fireKeys = this.input.keyboard.createCursorKeys();
     this.moveKeys = this.input.keyboard.addKeys({
       'up': Phaser.Input.Keyboard.KeyCodes.W,
@@ -30,19 +31,60 @@ export default class GameScene extends Phaser.Scene {
       'left': Phaser.Input.Keyboard.KeyCodes.A,
       'right': Phaser.Input.Keyboard.KeyCodes.D
     });
+
     this.startText = this.add.bitmapText(this.width / 2, this.height / 2 - 200, 'arcadeFont', '', 75)
-      .setTint(0xFFFFFF)
-      .setOrigin(0.5, 0.5);
+    .setTint(0xFFFFFF)
+    .setOrigin(0.5, 0.5);
+
+    this.doorLeft = this.physics.add.sprite(this.width / 2 - 70 , -180)
+    this.physics.add.existing(this.doorLeft);
+    this.doorLeft.body
+      .setSize(10, 600)
+      .setImmovable(true);
+
+    this.doorRight = this.physics.add.sprite(this.width / 2 + 60 , -180)
+    this.physics.add.existing(this.doorRight);
+    this.doorRight.body
+      .setSize(10, 600)
+      .setImmovable(true);
+
+    this.physics.add.existing(this.doorRight);
+    this.doorRight.body
+      .setSize(10, 600)
+      .setImmovable(true);
+
+    this.borderDark = this.add.graphics()
+      .fillStyle(0x000000, 1)
+      .fillRect(0, this.height / 2 - 265, 50, this.height - 169)
+      .fillRect(this.width - 50, this.height / 2 - 265, 50, this.height - 169)
+      .fillRect(this.width / 2 - 82, 0, 150, 50)
+      .fillRect(0, this.height - 50, this.width, 50)
+      .setDepth(2);
+
+    this.borderLight = this.add.graphics()
+      .fillStyle(0x000000, 0.5)
+      .fillRect(0, this.height / 2 - 265, 75, this.height - 194)
+      .fillRect(this.width - 75, this.height / 2 - 265, 75, this.height - 194)
+      .fillRect(this.width / 2 - 82, 0, 150, 75)
+      .fillRect(0, this.height - 75, this.width, 75)
+      .setDepth(1);
+
     this.countdown();
     this.createGroups();
     this.createPlayer();
     this.addCollisions();
     this.setupEvents();
-    this.physics.world.setBounds(0, 100, 1336, 768);
+    this.physics.world.setBounds(0, 120, this.width, this.height - 100);
     this.playerAvatar = this.add.sprite(1115, 32, 'playerAvatar')
-      .setScale(0.5);
-    this.scoreText = this.add.bitmapText(20, 20, 'arcadeFont', 'Score:0', 25).setTint(0xFFFFFF);
-    this.livesText = this.add.bitmapText(1140, 20, 'arcadeFont', `Lives:${this.player.lives}`, 25).setTint(0xFFFFFF);
+      .setScale(0.5)
+      .setDepth(1);
+    this.scoreText = this.add.bitmapText(20, 20, 'arcadeFont', 'Score:0', 25)
+      .setTint(0xFFFFFF)
+      .setDepth(1);
+    this.livesText = this.add.bitmapText(1140, 20, 'arcadeFont', `Lives:${this.player.lives}`, 25)
+      .setTint(0xFFFFFF)
+      .setDepth(1);
+
   }
   setupEvents() {
     this.events.on('gameover', () => {
@@ -62,6 +104,12 @@ export default class GameScene extends Phaser.Scene {
   }
   addCollisions() {
     this.physics.add.collider(this.itBugs, this.itBugs);
+    this.physics.add.collider(this.itMonsters, this.itMonsters);
+    this.physics.add.collider(this.itMonsters, this.itBugs);
+
+    this.physics.add.collider(this.itMonsters, this.doorLeft);
+    this.physics.add.collider(this.itMonsters, this.doorRight);
+
     this.physics.add.overlap(this.player, this.itBugs, () => this.player.onHit(this.collisionDamage, this.livesText), this.checkitBugCollision, this);
     this.physics.add.overlap(this.itBugs, this.bullets, this.bulletCollision, this.checkBulletCollision, this);
   }
@@ -156,7 +204,7 @@ export default class GameScene extends Phaser.Scene {
           .setVisible(true)
           .setScale(0.6)
           .spawn(this.width / 2, 0);
-        let newTime = Phaser.Math.Between(500, 1500);
+        let newTime = Phaser.Math.Between(1500, 3000);
         this.spawnTimerMonster = time + newTime;
       }
     }
