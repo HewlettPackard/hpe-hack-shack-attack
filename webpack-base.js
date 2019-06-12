b3f1da5bcece4e0ac0ab0e5dcc02fec2dcc72313
Madhu Rebbana <madhu.rebbana@gmail.com>
2019-06-13 00:57:09 +0530

    dist folder added for netlify deployment

--- webpack-base.js
+++ webpack-base.js
@@ -26,9 +26,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(["dist"], {
-      root: path.resolve(__dirname, "./")
-    }),
     new webpack.DefinePlugin({
       CANVAS_RENDERER: JSON.stringify(true),
       WEBGL_RENDERER: JSON.stringify(true)
